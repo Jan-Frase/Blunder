78b5d8baaa7b692e8bae732389eca9a69d560189
Jan Frase <mrunkioiw@gmail.com>
2025-06-07 21:52:33 +0200

    Refactored PerftRunner a bit.

--- src/main/java/de/janfrase/blunder/Main.java
+++ src/main/java/de/janfrase/blunder/Main.java
@@ -1,44 +1,8 @@
 /* Made by Jan Frase :) */
 package de.janfrase.blunder;
 
-import de.janfrase.blunder.engine.movegen.MoveGenerator;
-import de.janfrase.blunder.engine.movegen.move.Move;
-import de.janfrase.blunder.engine.state.game.GameState;
-import de.janfrase.blunder.engine.state.game.GameStateFenParser;
-import de.janfrase.blunder.engine.state.game.GameStatePrinter;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.config.Configurator;
-
 public class Main {
-    private static final Logger logger = LogManager.getLogger();
-
     public static void main(String[] args) {
-        GameStateFenParser.loadFenString(
-                "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
-
-        // this produces the correct result
-        // Move move = UciMoveParser.parseUciMove("a1b1");
-
-        // this as well -> thus the problem lies in me not resetting some of the state
-        Move move =
-                MoveGenerator.generatePseudoLegalMoves().stream()
-                        .filter(m -> m.toString().equals("a1b1"))
-                        .findFirst()
-                        .orElseThrow();
-
-        GameState.getInstance().makeMove(move);
-
-        // int nodes = PerftRunner.perft(3);
-
-        long nodes =
-                PerftRunner.perft(
-                        4, "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1");
-
-        System.out.println(nodes);
-
-        Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.TRACE);
-        logger.trace(GameStatePrinter.print());
+        // Does nothing atm
     }
 }
--- src/main/java/de/janfrase/blunder/PerftRunner.java
+++ src/main/java/de/janfrase/blunder/PerftRunner.java
@@ -10,47 +10,90 @@ import de.janfrase.blunder.engine.state.game.GameStatePrinter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
+import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.core.config.Configurator;
 
 public class PerftRunner {
     private static final Logger logger = LogManager.getLogger();
     private static final GameState gameState = GameState.getInstance();
 
+    /**
+     * This way to call the engine should only be used to run perftree.
+     * <a href="https://github.com/agausmann/perftree">Perftree GitHub</a>
+     *
+     * @param args Command line arguments in the following order:
+     *             <ul>
+     *             <li>depth - The depth parameter of the perft function</li>
+     *             <li>fen - The Forsyth-Edwards Notation string of the base position</li>
+     *             <li>moves - Optional space-separated sequence of moves from the base position.
+     *                 Moves are in UCI notation (source-target-promotion), where castling is encoded
+     *                 as the king's move. Examples: e2e4, e1g1, a7b8Q</li>
+     *             </ul>
+     */
     public static void main(String[] args) {
+        // Disable all logging (aside from errors). Has to be done because PerfTree relies on what
+        // we print.
+        // We could still continue logging to a file, but it's quite slow.
+        Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.ERROR);
+
         int depths = Integer.parseInt(args[0]);
         String fenString = args[1];
 
+        // Extract the move strings...
         String[] moveStrings = new String[args.length - 2];
         System.arraycopy(args, 2, moveStrings, 0, moveStrings.length);
 
+        // ...and convert them to move records.
         Move[] moves =
                 Arrays.stream(moveStrings)
                         .map(UciMoveParser::parseUciMove)
                         .toList()
                         .toArray(new Move[0]);
 
-        perft(depths, fenString, moves);
-    }
-
-    public static long perft(int depths, String fenString, Move... moves) {
+        // Load the starting position
         GameStateFenParser.loadFenString(fenString);
 
+        // Make all moves that have to be applied before perft can begin.
         for (Move move : moves) {
             gameState.makeMove(move);
         }
 
-        long nodes = 0;
+        // Let's go :)
+        perftWithPrint(depths);
+    }
 
+    /**
+     * Executes a perft search but also prints the results in the format required by perftree.
+     * <p>
+     * Should look something like this:
+     * <p>
+     * a2a3 380
+     * <p>
+     * b2b3 420
+     * <p>
+     * c2c3 420
+     * <p>
+     * 8902
+     *
+     * @param depths How deep we should search. Scales exponentially.
+     */
+    private static void perftWithPrint(int depths) {
+        long total_nodes = 0;
         ArrayList<Move> legalMoves = MoveGenerator.generateLegalMoves();
+
+        // Since the perftree tool also sorts the moves, it can be helpful to have them in the same
+        // order.
         legalMoves.sort(Comparator.comparing(Move::toString));
         for (Move move : legalMoves) {
+            // TODO: Remove hashing logic.
             int preHash = gameState.hashCode();
             gameState.makeMove(move);
             logger.info(GameStatePrinter.print());
 
             long current_nodes = perft(depths - 1);
-            nodes += current_nodes;
+            total_nodes += current_nodes;
             System.out.println(move.toString() + " " + current_nodes);
 
             gameState.unmakeMove(move);
@@ -60,18 +103,23 @@ public class PerftRunner {
         }
 
         System.out.println();
-        System.out.println(nodes);
-        return nodes;
+        System.out.println(total_nodes);
     }
 
+    /**
+     * Starts a perft test from the current position.
+     * <a href="https://www.chessprogramming.org/Perft">Wiki entry.</a>
+     * @param depths How deep we should search. Scales exponentially.
+     * @return The number of positions discovered.
+     */
     public static long perft(int depths) {
-        // Configurator.setAllLevels(LogManager.getRootLogger().getName(), Level.ERROR);
-
         long nodes = 0;
 
         if (depths == 0) {
             return 1;
         }
+        // TODO: If the depths == 1 we can simply return the length of this list. -> Easy
+        // performance gains.
         ArrayList<Move> moves = MoveGenerator.generatePseudoLegalMoves();
 
         for (Move move : moves) {
