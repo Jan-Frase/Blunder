e4f2e2e53d6a230d2f56aefdbe2f98f92151a915
Jan Frase <mrunkioiw@gmail.com>
2025-08-14 17:08:23 +0200

    Fix: Various uci improvements.

--- src/main/java/de/janfrase/blunder/uci/UciParser.java
+++ src/main/java/de/janfrase/blunder/uci/UciParser.java
@@ -5,19 +5,38 @@ import de.janfrase.blunder.engine.backend.movegen.MoveGenerator;
 import de.janfrase.blunder.engine.backend.movegen.move.Move;
 import de.janfrase.blunder.engine.backend.state.game.FenParser;
 import de.janfrase.blunder.engine.backend.state.game.GameState;
+import de.janfrase.blunder.utility.Constants;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class UciParser {
 
+    // singleton pattern
     private static final UciParser INSTANCE = new UciParser();
 
+    // logging
+    private static final Logger LOGGER = LogManager.getLogger();
+    private static final String IN = "In: ";
+    private static final String OUT = "Out: ";
+
+    // incoming commands
     private static final String UCI = "uci";
     private static final String QUIT = "quit";
     private static final String UCI_NEW_GAME = "ucinewgame";
     private static final String POSITION = "position";
     private static final String GO = "go";
     private static final String STOP = "stop";
+    private static final String IS_READY = "isready";
+
+    // outgoing messages
+    private static final String ID = "id Blunder";
+    private static final String UCI_OK = "uciok";
+    private static final String READY_OK = "readyok";
+    private static final String BEST_MOVE = "bestmove";
 
     private UciParser() {}
 
@@ -40,6 +59,8 @@ public class UciParser {
             scanner.hasNextLine();
             String input = scanner.nextLine();
 
+            LOGGER.info(IN + input);
+
             String[] tokens = input.split(" ");
             String commandName = tokens[0];
 
@@ -52,17 +73,28 @@ public class UciParser {
                 case POSITION -> position(arguments);
                 case GO -> go(arguments);
                 case STOP -> stop();
+                case IS_READY -> isReady();
             }
         }
     }
 
+    private void isReady() {
+        sendReply(READY_OK);
+    }
+
+    private void sendReply(String message) {
+        System.out.println(message);
+        LOGGER.info(OUT + message);
+    }
+
     private void uci() {
-        System.out.println("id Blunder");
-        System.out.println("uciok");
+        sendReply(ID);
+        sendReply(UCI_OK);
     }
 
     private void uciNewGame() {
         GameState.resetGameState();
+        sendReply(READY_OK);
     }
 
     private void position(String[] arguments) {
@@ -80,7 +112,7 @@ public class UciParser {
         for (int i = 0; i < indexOfMovesKeyword; i++) {
             fenStringBuilder.append(arguments[i]).append(" ");
         }
-        String fenString = fenStringBuilder.toString();
+        String fenString = fenStringBuilder.toString().strip();
 
         if (fenString.equals("startpos")) {
             FenParser.loadStartingPosition();
@@ -93,6 +125,8 @@ public class UciParser {
             Move move = UciMoveParser.parseUciMove(moveString);
             GameState.getInstance().makeMove(move);
         }
+
+        LOGGER.info("finished position command");
     }
 
     private void go(String[] arguments) {
@@ -112,6 +146,14 @@ public class UciParser {
     }
 
     private void stop() {
-        System.out.println(MoveGenerator.generateLegalMoves().getFirst().toString());
+        List<Move> moves = MoveGenerator.generateLegalMoves();
+        List<Move> captureMoves =
+                moves.stream()
+                        .filter(move -> move.capturedPieceType() != Constants.PieceType.EMPTY)
+                        .toList();
+        Move randomMove = moves.getFirst();
+
+        Move playedMove = !captureMoves.isEmpty() ? captureMoves.getFirst() : randomMove;
+        sendReply(BEST_MOVE + " " + playedMove.toString());
     }
 }
